065746e2b6b4ca0c15ff69ffa3b640f92939ca8c
Bor1ss <b.kulagin@gmail.com>
2022-01-13 13:50:22 +0000

    Automated Release $(Release.ReleaseId)

---
+++ _nuxt/static/1642081792/it/purchase/upgrades/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("it/purchase/upgrades", (function(a){return {data:[{payload:{Title:"UPGRADES",Id:a,Link:"upgrades",Order:a,MaxCards:1,H4:"SELECT_UPGRADE_OR_LIFETIME",TextBody1:"EVERY_USER_ACCOUNT_ON_EACH_WORKSTATION_NEEDS_AN_INDIVIDUAL_LICENSE",MinCards:0,ShortH5:"SELECT_UPGRADE_OR_LIFETIME_SHORT"}}],fetch:{},mutations:[]}}(2)));
