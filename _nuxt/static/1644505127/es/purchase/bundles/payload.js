933b0a6135faad78acd9b8ba0e3c069d032ed374
Bor1ss <b.kulagin@gmail.com>
2022-02-10 14:59:19 +0000

    Automated Release $(Release.ReleaseId)

---
+++ _nuxt/static/1644505127/es/purchase/bundles/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("es/purchase/bundles", (function(a){return {data:[{payload:{Title:"BUNDLES",Id:a,Link:"bundles",Order:a,MaxCards:1,H4:"BUNDLE_AND_SAVE",TextBody1:"EVERY_USER_ACCOUNT_ON_EACH_WORKSTATION_NEEDS_AN_INDIVIDUAL_LICENSE",MinCards:0,ShortH5:"BUNDLE_AND_SAVE_SHORT"}}],fetch:{},mutations:[]}}(3)));
