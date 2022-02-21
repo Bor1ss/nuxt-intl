c57b03e035b12a2185da52fe6c702027587a81d6
Bor1ss <b.kulagin@gmail.com>
2022-02-21 12:41:28 +0000

    Automated Release $(Release.ReleaseId)

---
+++ _nuxt/static/1645447249/de/purchase/licenses/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/de/purchase/licenses", (function(a){return {data:[{payload:{Title:"LICENSES",Id:a,Link:"licenses",Order:a,MaxCards:a,H4:"STEP1_PAGE_H",TextBody1:"STEP1_PAGE_DESCRIPTION",MinCards:a,ShortH5:"STEP1_PAGE_H_SHORT"}}],fetch:{},mutations:[]}}(1)));
