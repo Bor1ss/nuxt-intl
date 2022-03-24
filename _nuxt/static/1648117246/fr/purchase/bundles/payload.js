94bcb6d4a1b7b5b2faf6217d586af045db975ecf
Bor1ss <b.kulagin@gmail.com>
2022-03-24 10:21:53 +0000

    Automated Release $(Release.ReleaseId)

---
+++ _nuxt/static/1648117246/fr/purchase/bundles/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/fr/purchase/bundles", (function(a){return {data:[{payload:{Title:"BUNDLES",Id:a,Link:"bundles",Order:a,MaxCards:1,H4:"STEP3_PAGE_H",TextBody1:"STEP3_PAGE_DESCRIPTION",MinCards:0,ShortH5:"STEP3_PAGE_H_SHORT"}}],fetch:{},mutations:[]}}(3)));
