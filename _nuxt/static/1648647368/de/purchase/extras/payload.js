dad101bcbb645b2442d92eccaa17f8d0d5dc5488
Bor1ss <b.kulagin@gmail.com>
2022-03-30 13:37:17 +0000

    Automated Release $(Release.ReleaseId)

---
+++ _nuxt/static/1648647368/de/purchase/extras/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/de/purchase/extras", (function(a){return {data:[{payload:{Title:"EXTRAS",Id:a,Link:"extras",Order:a,MaxCards:2,H4:"STEP4_PAGE_H",TextBody1:"STEP4_PAGE_DESCRIPTION",MinCards:0,ShortH5:"STEP4_PAGE_SHORT",MetaTitle:"STEP4_META_TITLE",MetaDescription:"STEP4_META_DESCRIPTION",MetaKeywords:"STEP4_META_KEYWORDS"}}],fetch:{},mutations:[]}}(4)));
