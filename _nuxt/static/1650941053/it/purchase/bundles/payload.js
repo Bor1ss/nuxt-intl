03c71d394ced4ed2c0408fd74b0815f307ea95b3
Bor1ss <b.kulagin@gmail.com>
2022-04-26 02:45:13 +0000

    Automated Release $(Release.ReleaseId)

---
+++ _nuxt/static/1650941053/it/purchase/bundles/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("it/purchase/bundles", (function(a){return {data:[{payload:{Title:"STEPS_BAR_STEP3",Id:a,Link:"bundles",Order:a,MaxCards:1,H4:"STEP3_PAGE_H",TextBody1:"STEP3_PAGE_DESCRIPTION",MinCards:0,ShortH5:"STEP3_PAGE_H_SHORT",MetaTitle:"STEP3_META_TITLE",MetaDescription:"STEP3_META_DESCRIPTION",MetaKeywords:"STEP3_META_KEYWORDS"}}],fetch:{},mutations:[]}}(3)));
