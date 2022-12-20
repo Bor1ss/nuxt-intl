1e02ed1a83d1bece10c93f73354435c420a9be63
Bor1ss <b.kulagin@gmail.com>
2022-12-20 19:02:23 +0000

    Automated Release $(Release.ReleaseId)

---
+++ _nuxt/static/1671562888/de/purchase/licenses/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/de/purchase/licenses", (function(a){return {data:[{payload:{Title:"STEPS_BAR_STEP1",Id:a,Link:"licenses",Order:a,MaxCards:a,H4:"STEP1_PAGE_H",TextBody1:"STEP1_PAGE_DESCRIPTION",MinCards:a,ShortH5:"STEP1_PAGE_H_SHORT",MetaTitle:"STEP1_META_TITLE",MetaDescription:"STEP1_META_DESCRIPTION",MetaKeywords:"STEP1_META_KEYWORDS"}}],fetch:{},mutations:[]}}(1)));
